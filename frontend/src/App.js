793a2f147ede75f186d7de97ded94bf3404935ac
Kevin Alves <56762529+KevinAlvesJS@users.noreply.github.com>
2020-03-08 18:34:51 -0300

    update frontendfolder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,12 @@ import logo from './assets/logo.svg'
 function App() {
   const [email, setEmail] = useState('')
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
-    console.log(email);
+    
+   const response = await api.post('/sessions', { email })
+    
+   console.log(response);
     
   }
   return (
