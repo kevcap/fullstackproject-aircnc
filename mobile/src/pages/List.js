897f353fa2c4fd3765144fd63e7f3db7d7208529
Kevin Alves <56762529+KevinAlvesJS@users.noreply.github.com>
2020-03-17 16:46:19 -0300

    mobile | style #2: finished style on List.js
    - Add comany images;
    - Arranged everything on lists;
    - Add book button;

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { SafeAreaView, Text, Image, AsyncStorage, StyleSheet } from 'react-native'
+import { SafeAreaView, ScrollView, Text, Image, AsyncStorage, StyleSheet } from 'react-native'
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
@@ -15,6 +19,9 @@ export default function List() {
   return (
     <SafeAreaView style={styles.container}>
       <Image style={styles.logo} source={logo} />
+      <ScrollView>
+        {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+      </ScrollView>
     </SafeAreaView>
   );
 };
