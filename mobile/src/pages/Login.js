933a53e06bf14a286992b226549c6371b0531f0f
Kevin Alves <56762529+KevinAlvesJS@users.noreply.github.com>
2020-03-17 15:31:49 -0300

    mobile | feat #5: Add navigation from Login.js to List.js

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
-import { View, KeyboardAvoidingView, Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage, KeyboardAvoidingView, Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import api from '../services/api'
 
 import logo from '../assets/logo.png';
 
-export default function Login() {
+export default function Login({ navigation }) {
   const [email, setEmail] = useState('')
   const [techs, setTechs] = useState('')
   
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if(user) {
+        navigation.navigate('List');
+      }
+    })
+  }, [])
+
   async function handleSubmit(){
     const response = await api.post('/sessions', { email })
 
     const { _id } = response.data;
 
-    console.log(_id);
+    await AsyncStorage.setItem('user', _id);
+    await AsyncStorage.setItem('tech', techs);
     
+    navigation.navigate('List');
   }
   return (
   <KeyboardAvoidingView behavior="padding" style={styles.container}>
