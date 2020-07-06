e03415f571e26541b2abd033275d1aa2159cd6dc
Kevin Alves <56762529+KevinAlvesJS@users.noreply.github.com>
2020-07-05 21:20:30 -0300

    🎨 STYLE: Update frontend CSS

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,37 +1,39 @@
-import React, { useState } from 'react'
-import api from '../../services/api'
+import React, { useState } from 'react';
+import api from '../../services/api';
 
-export default function Login( { history } ) {
-    const [email, setEmail] = useState('')
+export default function Login({ history }) {
+  const [email, setEmail] = useState('');
 
-    async function handleSubmit(event) {
-      event.preventDefault();
-  
-      const response = await api.post('/sessions', { email });
-      const { _id } = response.data;
-  
-      localStorage.setItem('user', _id);
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-      history.push('/dashboard') //sends user to dashboard
-    }
+    const response = await api.post('/sessions', { email });
 
-    return (
-        <>
-            <p>
-            Offer <strong>spots</strong> to developers and find <strong>talents</strong> to your company
-            </p>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">E-MAIL *</label>
-                <input
-                    type="email"
-                    id="email"
-                    placeholder="Your best e-mail"
-                    value={email}
-                    onChange={event => setEmail(event.target.value)}
-                />
+    const { _id } = response.data;
 
-                <button className="btn" type="submit">Sign In</button>
-            </form>
-        </>
-    )
+    localStorage.setItem('user', _id);
+
+    history.push('/dashboard');
+  }
+
+  return (
+    <>
+      <p>
+        Offer <strong>spots</strong> to developers and find <strong>talents</strong> to your company
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-MAIL *</label>
+        <input 
+          id="email" 
+          type="email" 
+          placeholder="Your best email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+
+        <button className="btn" type="submit">Login</button>
+      </form>
+    </>
+  )
 }
--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,70 +1,74 @@
 import React, { useState, useMemo } from 'react';
 import api from '../../services/api';
-import './style.css';
 
-import camera from '../../assets/camera.svg'
+import camera from '../../assets/camera.svg';
+
+import './styles.css';
 
 export default function New({ history }) {
-    const [thumbnail, setThumbnail] = useState(null);
-    const [company, setCompany] = useState('');
-    const [techs, setTechs] = useState('');
-    const [price, setPrice] = useState('');
-    
-    const preview = useMemo(() => {
-        return thumbnail ? URL.createObjectURL(thumbnail) : null
-    }, [thumbnail])
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const data = new FormData();
-        const user_id = localStorage.getItem('user');
-        
-        data.append('thumbnail', thumbnail);
-        data.append('company', company);
-        data.append('techs', techs);
-        data.append('price', price);
+  const [thumbnail, setThumbnail] = useState(null);
+  const [company, setCompany] = useState('');
+  const [techs, setTechs] = useState('');
+  const [price, setPrice] = useState('');
 
-       await api.post('/spots', data, {
-            headers: { user_id }
-        });
-        history.push('/dashboard');
-    }
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail])
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label 
-                id="thumbnail"
-                style={{backgroundImage: `url(${preview})`}}
-                className={thumbnail ? 'has-thumbnail' : ''}
-            >
-                <input type="file" onChange={ event => setThumbnail(event.target.files[0])} />
-                <img src={camera} alt="Select svg"/>
-            </label>
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-            <label htmlFor="company">COMPANY *</label>
-            <input
-                id="company"
-                placeholder="Your amazing company"
-                value={company}
-                onChange={event => setCompany(event.target.value)}
-            />
+    const data = new FormData();
+    const user_id = localStorage.getItem('user');
 
-            <label htmlFor="techs">TECHS * <span>(separeted by commas)</span></label>
-            <input
-                id="techs"
-                placeholder="What technologies do you use?"
-                value={techs}
-                onChange={event => setTechs(event.target.value)}
-            />
+    data.append('thumbnail', thumbnail);
+    data.append('company', company);
+    data.append('techs', techs);
+    data.append('price', price);
 
-            <label htmlFor="price">DAILY VALUE * <span>(empty if it's FREE)</span></label>
-            <input
-                id="price"
-                placeholder="Value charged per day"
-                value={price}
-                onChange={event => setPrice(event.target.value)}
-            />
+    await api.post('/spots', data, {
+      headers: { user_id }
+    })
 
-            <button className="btn">Register</button>
-        </form>
-    )
+    history.push('/dashboard');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label 
+        id="thumbnail" 
+        style={{ backgroundImage: `url(${preview})` }}
+        className={thumbnail ? 'has-thumbnail' : ''}
+      >
+        <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
+        <img src={camera} alt="Select img" />
+      </label>
+
+      <label htmlFor="company">COMPANY *</label>
+      <input 
+        id="company"
+        placeholder="Sua empresa incrível"
+        value={company}
+        onChange={event => setCompany(event.target.value)}
+      />
+
+      <label htmlFor="techs">TECHNOLOGIES * <span>(comma-separated)</span></label>
+      <input 
+        id="techs"
+        placeholder="Which tech do you use?"
+        value={techs}
+        onChange={event => setTechs(event.target.value)}
+      />
+
+      <label htmlFor="price">DAILY VALUE * <span>(blank if FREE)</span></label>
+      <input 
+        id="price"
+        placeholder="Value charged daily"
+        value={price}
+        onChange={event => setPrice(event.target.value)}
+      />
+
+      <button type="submit" className="btn">Register</button>
+    </form>
+  )
 }
